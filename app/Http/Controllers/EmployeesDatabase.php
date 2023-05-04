247f4fa007996b35d1e26f1dd0b61fdb4a762d79
Dio Febrian <diofebrian42@gmail.com>
2023-05-04 15:18:58 +0700

    filter done

--- app/Http/Controllers/EmployeesDatabase.php
+++ app/Http/Controllers/EmployeesDatabase.php
@@ -7,9 +7,22 @@ use Illuminate\Http\Request;
 
 class EmployeesDatabase extends Controller
 {
-    public function index()
+    public function index(Request $request)   
     {
-        $listUser = User::all();
+        // $listUser = User::with('users_detail');
+        // $positionIds = explode(',', $request->position_id);
+        // $listUser = User::join('users_details', 'users.id', '=', 'users_details.user_id')
+        // ->whereIn('users_details.position_id', $positionIds)
+        //     ->get();
+        $positionIds = explode(',', $request->position_id);
+        $users = User::join('users_details', 'users.id', '=', 'users_details.user_id');
+
+        if (!empty($request->position_id)) {
+            $users->whereIn('users_details.position_id', $positionIds);
+        }
+
+        $listUser = $users->get();
+
         return view('management.database.employees', ['users' => $listUser]);
     }
 }
--- resources/views/management/database/employees.blade.php
+++ resources/views/management/database/employees.blade.php
@@ -37,42 +37,84 @@ active
     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold @role('freelancer') text-success @else text-primary @endrole" id="judul">Database</h6>
         <div class="text-right">
-            <button class="btn @role('freelancer') btn-success @else btn-danger @endrole btn-sm" type="button" id="manButton"><i class="fas fa-sort fa-sm text-white-50"></i> Sort by Consultant</button>
             <button class="btn @role('freelancer') btn-primary @else btn-success @endrole btn-sm" type="button" id="manButton"><i class="fas fa-download fa-sm text-white-50"></i> Export Selected</button>
         </div>
     </div>
+    <form action="/manage/list/employees/" method="GET">
+        @csrf
+        <div class="card-body">
+            <div class="col-md-12 zoom90">
+                <div class="row d-flex justify-content-start">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="position_id">Filter by Position:</label>
+                            <select name="position_id" class="form-control" id="position_id">
+                                <option value="">All Positions</option>
+                                <option value="4,18,19" {{ request('position_id') == '4,18,19' ? 'selected' : '' }}>Consultant</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-1 d-flex justify-content-center align-items-end">
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Filter"/>
+                        </div>
+                    </div>
+                    <div class="col-md-12"><br>
+                        <div class="table-responsive">
+                            <table class="table table-bordered zoom90" id="myProjects" width="100%" cellspacing="0">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th class="text-center">
+                                            <div class="form-check form-check-inline larger-checkbox">
+                                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                            </div>
+                                        </th>
+                                        <th>User ID</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                    <tr>
+                                        <td class="text-center">
+                                            <div class="form-check form-check-inline larger-checkbox">
+                                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                            </div>
+                                        </td>
+                                        <td>{{ $user->user_id }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                    </tr>
+                                    @endforeach
+                                 </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+{{-- <div class="card shadow mb-4">
+    
     <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered zoom90" id="myProjects" width="100%" cellspacing="0">
-                <thead class="thead-light">
-                    <tr>
-                        <th class="text-center">
-                            <div class="form-check form-check-inline larger-checkbox">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                            </div>
-                        </th>
-                        <th>User ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
-                    <tr>
-                        <td class="text-center">
-                            <div class="form-check form-check-inline larger-checkbox">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                            </div>
-                        </td>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                    </tr>
-                    @endforeach
-                    
-                </tbody>
-            </table>
+    <!-- Form filter -->
+    <form action="/manage/list/employees/" method="GET">
+    @csrf
+   <div class="col-md-12 zoom90">
+        <div class="row">
+            <div class="form-group">
+                <label for="position_id">Filter by Position:</label>
+                <select name="position_id" class="form-control" id="position_id">
+                    <option value="">All Positions</option>
+                    <option value="4,18,19" {{ request('position_id') == '4,18,19' ? 'selected' : '' }}>Consultant</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-download fa-sm text-white-50"></i> Filter</button>
         </div>
     </div>
-</div>
+    </form>
+    </div>
+</div> --}}
 @endsection
