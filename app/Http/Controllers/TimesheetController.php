7b049800dc81669c7cc3bb3686a154d1b61c4010
Haekal Sastradilaga <haekalboedoet@gmail.com>
2023-05-27 18:45:20 +0700

    asd

--- app/Http/Controllers/TimesheetController.php
+++ app/Http/Controllers/TimesheetController.php
@@ -369,20 +369,7 @@ class TimesheetController extends Controller
         if ($checkRole === null) {
             $totalIncentive = 0;
         } elseif ($checkRole === "MT") {
-            $mt_hiredDate = Users_detail::where('user_id', $user->id)
-                ->value('hired_date');
-            
-            // Assuming the hired_date is in the format 'Y-m-d' (e.g., 2022-02-04)
-            $hiredDate = new DateTime($mt_hiredDate);
-            $currentDate = new DateTime(date('Y-m-d'));
-            $intervalDate = $hiredDate->diff($currentDate);
-            $totalMonthsDifference = ($intervalDate->format('%y') * 12) + $intervalDate->format('%m');
-            
-            if ($totalMonthsDifference > 6 && $totalMonthsDifference <= 37) {
-                $roleFare = Additional_fare::where('id', $totalMonthsDifference > 24 ? 3 : ($totalMonthsDifference > 12 ? 2 : 1))
-                    ->value('fare');
-                $totalIncentive = $roleFare * 0.7;
-            }
+            $totalIncentive = 10 * 0.7;
         } else {
             $roleFare = Project_role::where('role_code', $checkRole)
                 ->value('fare');
