27a76effad103ce9d0788755a86721c1adb1b7bf
Haekal Sastradilaga <haekalboedoet@gmail.com>
2023-05-27 18:43:34 +0700

    asdds

--- app/Http/Controllers/TimesheetController.php
+++ app/Http/Controllers/TimesheetController.php
@@ -361,36 +361,34 @@ class TimesheetController extends Controller
         $entry->ts_activity = $request->activity;
         $entry->ts_status_id = '10';
 
-        $user = Auth::user()->id;
-        $checkRole = Project_assignment_user::where('user_id', $user)
-        ->where('project_assignment_id', $id_project)
-        ->pluck('role')
-        ->first();
+        $user = Auth::user();
+        $checkRole = Project_assignment_user::where('user_id', $user->id)
+            ->where('project_assignment_id', $id_project)
+            ->value('role');
+
         if ($checkRole === null) {
             $totalIncentive = 0;
         } elseif ($checkRole === "MT") {
-            $mt_hiredDate = Users_detail::where('user_id', Auth::user()->id)
-                ->pluck('hired_date')
-                ->first();
-        
+            $mt_hiredDate = Users_detail::where('user_id', $user->id)
+                ->value('hired_date');
+            
             // Assuming the hired_date is in the format 'Y-m-d' (e.g., 2022-02-04)
             $hiredDate = new DateTime($mt_hiredDate);
             $currentDate = new DateTime(date('Y-m-d'));
             $intervalDate = $hiredDate->diff($currentDate);
             $totalMonthsDifference = ($intervalDate->format('%y') * 12) + $intervalDate->format('%m');
-        
+            
             if ($totalMonthsDifference > 6 && $totalMonthsDifference <= 37) {
                 $roleFare = Additional_fare::where('id', $totalMonthsDifference > 24 ? 3 : ($totalMonthsDifference > 12 ? 2 : 1))
-                    ->pluck('fare')
-                    ->first();
+                    ->value('fare');
                 $totalIncentive = $roleFare * 0.7;
             }
         } else {
             $roleFare = Project_role::where('role_code', $checkRole)
-                ->pluck('fare')
-                ->first();
+                ->value('fare');
             $totalIncentive = $roleFare * 0.7;
         }
+
         $fare = Project_location::where('location_code', $request->location)->pluck('fare')->first();
         $countAllowances = $fare;
 
