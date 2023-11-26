462d7bfdc1f6fc42bf45fc2e2bfa3771053339aa
Haekal Sastradilaga <haekalboedoet@gmail.com>
2023-11-27 03:53:36 +0700

    sd

--- resources/views/news-feed/edit_post.blade.php
+++ resources/views/news-feed/edit_post.blade.php
@@ -57,9 +57,9 @@ active
 <script>
     tinymce.init({
     selector: 'textarea',
-    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
     toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-    // tinycomments_mode: 'embedded',
+    tinycomments_mode: 'embedded',
     tinycomments_author: 'Author name',
     mergetags_list: [
       { value: 'First.Name', title: 'First Name' },
