24b1b03426d786f3877019f3c628204e6b2c952a
Haekal Sastradilaga <haekalboedoet@gmail.com>
2024-01-23 12:49:12 +0700

    asd

--- app/Jobs/CheckEmploymentStatus.php
+++ app/Jobs/CheckEmploymentStatus.php
@@ -66,12 +66,12 @@ class CheckEmploymentStatus implements ShouldQueue
 
         // Customize this logic based on your requirements
         $notificationDate = now()->subDays(30); // 30 days before contract end
-        $recipients = ['haekal@perdana.co.id', 'dio@perdana.co.id'];
+        $recipients = ['sundari@perdana.co.id', 'suryadi@perdana.co.id'];
 
         if ($endDate->isAfter($notificationDate))
         {
             $notification = new EmploymentStatusNotification($user, 'Contract', $hiredDate, $HrName);
-            Mail::to('haekal@perdana.co.id')->cc($recipients)->send($notification);
+            Mail::to('hrd@perdana.co.id')->cc($recipients)->send($notification);
         }
     }
 
@@ -81,12 +81,12 @@ class CheckEmploymentStatus implements ShouldQueue
 
         // Customize this logic based on your requirements
         $notificationDate = $hiredDate->addMonths(2); // 2 months after the hired date
-        $recipients = ['haekal@perdana.co.id', 'dio@perdana.co.id'];
+        $recipients = ['sundari@perdana.co.id', 'suryadi@perdana.co.id'];
 
         if (now()->isAfter($notificationDate))
         {
             $notification = new EmploymentStatusNotification($user, 'Probation', $hiredDate, $HrName);
-            Mail::to('haekal@perdana.co.id')->cc($recipients)->send($notification);
+            Mail::to('hrd@perdana.co.id')->cc($recipients)->send($notification);
         }
     }
 
@@ -97,20 +97,20 @@ class CheckEmploymentStatus implements ShouldQueue
         // Customize this logic based on your requirements
         $firstNotificationDate = $hiredDate->addMonths(2); // 2 months after the hired date
         $secondNotificationDate = $hiredDate->addMonths(35); // 35 months after the hired date
-        $recipients = ['haekal@perdana.co.id', 'dio@perdana.co.id'];
+        $recipients = ['sundari@perdana.co.id', 'suryadi@perdana.co.id'];
 
         // Check for the first notification
         if (now()->isAfter($firstNotificationDate))
         {
             $notification = new EmploymentStatusNotification($user, 'Probation of Management Trainee Program', $hiredDate, $HrName);
-            Mail::to('haekal@perdana.co.id')->cc($recipients)->send($notification);
+            Mail::to('hrd@perdana.co.id')->cc($recipients)->send($notification);
         }
 
         // Check for the second notification
         if (now()->isAfter($secondNotificationDate))
         {
             $notification = new EmploymentStatusNotification($user, 'Management Trainee', $hiredDate, $HrName);
-            Mail::to('haekal@perdana.co.id')->cc($recipients)->send($notification);
+            Mail::to('hrd@perdana.co.id')->cc($recipients)->send($notification);
         }
     }
 }
