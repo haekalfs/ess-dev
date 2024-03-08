47b85d1783bebbca08fd9383cca29fba89bac22f
Haekal Sastradilaga <haekalboedoet@gmail.com>
2024-03-08 11:26:31 +0700

    adsad

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -184,7 +184,7 @@ class HomeController extends Controller
             if ($typeSelected == 1) {
                 $activitiesQuery->whereIn('ts_location', ['HO'])->whereRaw('TIME(ts_from_time) < ?', ['08:00:00']); // Filter for tasks in the HO location and before 8 AM
             } elseif ($typeSelected == 2) {
-                $activitiesQuery->whereIn('ts_location', $getCompanyProjectIds)
+                $activitiesQuery->whereIn('ts_task_id', $getCompanyProjectIds)
                     ->whereRaw('TIME(ts_from_time) < ?', ['18:00:00'])
                     ->whereIn('ts_user_id', $getUsersAssignment); // Filter for tasks in company projects, before 6 PM, and assigned users
             }
