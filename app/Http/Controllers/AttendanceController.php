10a4749f088ae185397f442773956bb05902501d
Haekal Sastradilaga <haekalboedoet@gmail.com>
2024-03-18 16:03:00 +0700

    asdsd

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -65,7 +65,8 @@ class AttendanceController extends Controller
         foreach ($rows as $key => $row) {
             if ($key === 0) continue; // Skip the header row
 
-            $date = $row[0]; // Assuming the date is in the first column
+            $originalDate = $row[0]; // Assuming the date is in the first column
+            $date = date('Y-m-d', strtotime($originalDate));
             $payroll = $row[1]; // Assuming payroll is in the second column
             $tapIn = $row[3]; // Assuming Tap in is in the third column
             $tapOut = $row[4]; // Assuming Tap out is in the fourth column
